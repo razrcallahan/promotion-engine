1da91bedbf36d6376b9ce4ed7e16f3d58af7df3f
Wasim Qamar <razr.callahan@gmail.com>
2021-05-12 23:29:28 +0200

    promotion interface docs

--- src/main/java/com/demo/promotion/domain/promotions/Promotion.java
+++ src/main/java/com/demo/promotion/domain/promotions/Promotion.java
@@ -4,6 +4,10 @@ import com.demo.promotion.domain.CartProduct;
 
 import java.util.List;
 
+/**
+ * Generic promotion interface
+ * Implement the interface to create more promotion types
+ */
 public interface Promotion {
     Double apply(List<CartProduct> cartProducts);
 }
