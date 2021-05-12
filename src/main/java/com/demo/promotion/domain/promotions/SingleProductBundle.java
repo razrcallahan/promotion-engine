c5a22b1e097e2d54be2bb4c463286a689f9aee95
Wasim Qamar <razr.callahan@gmail.com>
2021-05-12 23:23:12 +0200

    Singe product bundle docs

--- src/main/java/com/demo/promotion/domain/promotions/SingleProductBundle.java
+++ src/main/java/com/demo/promotion/domain/promotions/SingleProductBundle.java
@@ -33,15 +33,22 @@ public class SingleProductBundle implements Promotion {
      */
     @Override
     public Double apply(List<CartProduct> cartProducts) {
+        // filter products that are available in bundle
+        // filter products that hasn't been included in another promotion already
         return cartProducts.stream()
                 .filter(p -> p.getProduct().equals(product))
                 .filter(p -> !p.getPromotionApplied())
                 .mapToDouble(p -> {
+                    // get absolute bundle count
                     int totalBundles = (int) p.getQuantity() / bundleQuantity;
+
+                    // get products left out of the bundle
                     int remaining = p.getQuantity() - (totalBundles * bundleQuantity);
 
+                    // setting promotion applied to true
                     p.setPromotionApplied(Boolean.TRUE);
 
+                    // sum of total bundle prices and products not included in the bundle
                     return  (totalBundles * this.getPrice()) + (remaining * product.getPrice());
                 })
                 .sum();
